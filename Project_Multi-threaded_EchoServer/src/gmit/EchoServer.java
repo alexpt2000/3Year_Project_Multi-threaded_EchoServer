7d842f94f626066023439afca3c49ecd886dd534
alexpt2000gmit <g00317835@gmit.ie>
2016-12-27 22:12:34 +0000

    At this point i can display the last ten transaction and save and load all values from the file into the system

--- Project_Multi-threaded_EchoServer/src/gmit/EchoServer.java
+++ Project_Multi-threaded_EchoServer/src/gmit/EchoServer.java
@@ -28,9 +28,7 @@ public class EchoServer {
 	public static void main(String[] args) throws Exception {
 		ServerSocket m_ServerSocket = new ServerSocket(2004, 10);
 		int id = 0;
-		
-		
-		
+
 		while (true) {
 			Socket clientSocket = m_ServerSocket.accept();
 			ClientServiceThread cliThread = new ClientServiceThread(clientSocket, id++);
@@ -46,17 +44,16 @@ class ClientServiceThread extends Thread {
 	boolean running = true;
 	boolean exit;
 	boolean loginConfimation;
-	
+
 	ObjectOutputStream out;
 	ObjectInputStream in;
 
 	String filename = "loginData.dat";
 	String statements = "Statements.dat";
 
-	
 	// User Dada for register
 	ClientBean client = new ClientBean();
-	
+
 	BankBean account = new BankBean();
 
 	String clientName = "";
@@ -64,16 +61,16 @@ class ClientServiceThread extends Thread {
 	String clientACnumber = "";
 	String clientUserName = "";
 	String clientPassword = "";
-	
+
 	double balance = 0.00;
 	double transaction = 0.00;
+	
+	String lastTransactions = "";
 
 	ArrayList<ClientBean> clientsBank = new ArrayList<ClientBean>();
 	ArrayList<BankBean> moviments = new ArrayList<BankBean>();
-	
-	Hashtable<String, ArrayList<BankBean>> clientsStatements = new Hashtable<String, ArrayList<BankBean>>();
-	
 
+	Hashtable<String, ArrayList<BankBean>> clientsStatements = new Hashtable<String, ArrayList<BankBean>>();
 
 	ClientServiceThread(Socket s, int i) {
 		clientSocket = s;
@@ -90,116 +87,116 @@ class ClientServiceThread extends Thread {
 		}
 	}
 
-	
 	/**
 	 * This is the method which makes use of add values into a Arraylist.
 	 * 
-	 * @param add Strings ref fields represents the register to the system.
+	 * @param add
+	 *            Strings ref fields represents the register to the system.
 	 * @return clientsBank.
 	 */
-	public synchronized ArrayList<ClientBean> addCLientList(String clientName, String clientAddress, String clientACnumber, String clientUserName, String clientPassword) {
+	public synchronized ArrayList<ClientBean> addCLientList(String clientName, String clientAddress,
+			String clientACnumber, String clientUserName, String clientPassword) {
 
 		clientsBank.add(new ClientBean(clientName, clientAddress, clientACnumber, clientUserName, clientPassword));
-	
+
 		return clientsBank;
 	}
 
-	
 	/**
-	 * This is the method which makes use of load values from file where the data are storage and
-	 * load into a Arraylist.
+	 * This is the method which makes use of load values from file where the
+	 * data are storage and load into a Arraylist.
 	 * 
-	 * @param no param.
+	 * @param no
+	 *            param.
 	 * @return clientsBank.
 	 */
 	public ArrayList<ClientBean> loadClientsFile() throws FileNotFoundException {
 
 		System.out.println("Loading Array");
-		
+
 		Scanner scan = new Scanner(new File(filename));
 
 		while (scan.hasNextLine()) {
 			String line = scan.nextLine();
 			String[] clientValue = line.split(",");
-			
+
 			System.out.println(clientValue[0]);
-			
-			clientsBank.add(new ClientBean(clientValue[0], clientValue[1], clientValue[2], clientValue[3], clientValue[4]));
+
+			clientsBank.add(
+					new ClientBean(clientValue[0], clientValue[1], clientValue[2], clientValue[3], clientValue[4]));
 		}
 
 		scan.close();
 
 		return clientsBank;
 	}
-	
-	
+
 	/**
 	 * This is the method verify if the user exist into the system
 	 * 
-	 * @param verifyUser(String clientACnumber, String clientUserName, String clientPassword).
+	 * @param verifyUser(String
+	 *            clientACnumber, String clientUserName, String clientPassword).
 	 * @return boolean loginConfimation.
 	 */
-	
-	public boolean verifyUser(String clientACnumber, String clientUserName, String clientPassword){
-		
-		loginConfimation = false;
-		
-			for (int i = 0; i < clientsBank.size(); i++ ){
-				
-				if (clientACnumber.equals(clientsBank.get(i).getClientACnumber()) && clientUserName.equals(clientsBank.get(i).getClientUserName()) && clientPassword.equals(clientsBank.get(i).getClientPassword())) {
-					loginConfimation = true;
-					System.out.println("OK ");
-					
-					i = clientsBank.size();
-				}
 
+	public boolean verifyUser(String clientACnumber, String clientUserName, String clientPassword) {
+
+		loginConfimation = false;
+
+		for (int i = 0; i < clientsBank.size(); i++) {
+
+			if (clientACnumber.equals(clientsBank.get(i).getClientACnumber())
+					&& clientUserName.equals(clientsBank.get(i).getClientUserName())
+					&& clientPassword.equals(clientsBank.get(i).getClientPassword())) {
+				loginConfimation = true;
+				System.out.println("OK ");
+
+				i = clientsBank.size();
 			}
-	
+
+		}
+
 		return loginConfimation;
 	}
 
-	
-	
 	/**
-	 * This is the method which makes use of load values from file where the data are storage and
-	 * load into a Arraylist.
+	 * This is the method which makes use of load values from file where the
+	 * data are storage and load into a Arraylist.
 	 * 
-	 * @param no param.
+	 * @param no
+	 *            param.
 	 * @return clientsBank.
 	 */
 	public Hashtable<String, ArrayList<BankBean>> loadStatementsFile() throws FileNotFoundException {
 
 		System.out.println("Loading Array");
-		
+
 		Scanner scan = new Scanner(new File(statements));
 
 		while (scan.hasNextLine()) {
 			String line = scan.nextLine();
 			String[] clientValue = line.split(",");
-			
+
 			System.out.println(clientValue[0]);
-			
+
 			double trans = Double.parseDouble(clientValue[1]);
 			double bal = Double.parseDouble(clientValue[2]);
-			
-			
+
 			moviments.add(new BankBean(trans, bal));
-			
+
 			clientsStatements.put(clientValue[0], moviments);
-			
-			
+
 		}
 
 		scan.close();
 
 		return clientsStatements;
 	}
-	
 
 	public void run() {
-		
-		//BankBean account = new BankBean();
-		
+
+		// BankBean account = new BankBean();
+
 		System.out.println(
 				"Accepted Client : ID - " + clientID + " : Address - " + clientSocket.getInetAddress().getHostName());
 		try {
@@ -211,7 +208,7 @@ class ClientServiceThread extends Thread {
 					+ clientSocket.getInetAddress().getHostName());
 
 			sendMessage("Connection successful\n----------------------\n \nWelcome Bank of GMIT");
-			
+
 			loadClientsFile();
 
 			boolean finish = false;
@@ -234,140 +231,168 @@ class ClientServiceThread extends Thread {
 
 							sendMessage("Enter your A/C Number");
 							clientACnumber = (String) in.readObject();
-							
+
 							sendMessage("Enter your Username");
 							clientUserName = (String) in.readObject();
 
 							sendMessage("Enter your Password");
 							clientPassword = (String) in.readObject();
-							
+
 							loginConfimation = verifyUser(clientACnumber, clientUserName, clientPassword);
 
 							loginConfimation = true;
-							
+
 						} while (loginConfimation == false);
 
 						// sendMessage(clientsBank.toString());
-						//sendMessage(clientsBank.get(0).getClientName());
-						
+						// sendMessage(clientsBank.get(0).getClientName());
+
 						exit = false;
-						
+
 						// Load all values from statement file
-						loadStatementsFile(); 
-						
-						do{
-							sendMessage("\nOP 3 - Change customer details\nOP 4 - Make Lodgements\nOP 5 - Make Withdrawal\nOP 6 - View the last ten transactions\n\nOP 99 - Exit");
+						loadStatementsFile();
+
+						do {
+
+							double bal = 0.00;
+
+							for (Entry<String, ArrayList<BankBean>> entry : clientsStatements.entrySet()) {
+								String key = entry.getKey();
+
+								if (key.equals(clientACnumber)) {
+
+									for (BankBean val : entry.getValue()) {
+										bal = val.getBalance();
+									}
+								}
+
+							}
+
+							sendMessage("\n----------------------------------\n--> Bank AC Number.: " + clientACnumber
+									+ "\n--> Current balance.: " + bal
+									+ "\n\nOP 3 - Change customer details\nOP 4 - Make Lodgements\nOP 5 - Make Withdrawal\nOP 6 - View the last ten transactions\n\nOP 99 - Exit\n\n" + lastTransactions);
 							message = (String) in.readObject();
 							
-							//Change customer details
-							if (message.compareTo("3") == 0){
-								
+							
+							lastTransactions = "";
+							
+							// Change customer details
+							if (message.compareTo("3") == 0) {
+
 							}
-	
+
 							// Make Lodgements
-							if (message.compareTo("4") == 0){
-								
+							if (message.compareTo("4") == 0) {
+
 								FileWriter fw = new FileWriter(statements, true);
 								BufferedWriter bw = new BufferedWriter(fw);
 								PrintWriter appendFileStatements = new PrintWriter(bw);
-								
+
 								sendMessage("Enter amount Lodgement");
 								String amount = (String) in.readObject();
-								
+
 								double value = Double.parseDouble(amount);
-								
+
 								balance = balance + value;
 								transaction = value;
-								
-								//logment(amount);
-								
+
+								// logment(amount);
+
 								appendFileStatements.println(clientACnumber + "," + transaction + "," + balance);
 
-								
 								moviments.add(new BankBean(transaction, balance));
-								
+
 								clientsStatements.put(clientACnumber, moviments);
-								
-								
 
 								appendFileStatements.close();
 								bw.close();
 								fw.close();
 
-								
 							}
-							
+
 							// Make Withdrawal
-							if (message.compareTo("5") == 0){
-								
-								
+							if (message.compareTo("5") == 0) {
+
 								FileWriter fw = new FileWriter(statements, true);
 								BufferedWriter bw = new BufferedWriter(fw);
 								PrintWriter appendFileStatements = new PrintWriter(bw);
-								
+
 								double value;
-								
-								do{
+
+								do {
 									sendMessage("Enter amount Withdrawal (limit 1000.00)");
 									String amount = (String) in.readObject();
-	
+
 									value = Double.parseDouble(amount);
-								
+
 								} while (value > 1000 && value > balance);
-							
+
 								balance = balance - value;
-			
+
 								value *= -1; // Convert to a negative number
-								
+
 								transaction = value;
-								
+
 								appendFileStatements.println(clientACnumber + "," + transaction + "," + balance);
-								
-								
+
 								moviments.add(new BankBean(transaction, balance));
-								
+
 								clientsStatements.put(clientACnumber, moviments);
-			
+
 								appendFileStatements.close();
 								bw.close();
 								fw.close();
-								
+
 							}
-							
+
 							// View the last ten transactions
-							if (message.compareTo("6") == 0){
-								
+							if (message.compareTo("6") == 0) {
+
 								System.out.println(balance);
 								System.out.println(transaction);
-								
-								//System.out.println(clientsStatements.get(clientACnumber).listIterator());
-			
-								
-								
-//							    Set set = clientsStatements.entrySet();
-//							    Iterator it = set.iterator();
-//							    while (it.hasNext()) {
-//							      Map.Entry entry = (Map.Entry) it.next();
-//							      System.out.println(entry.getKey() + " : " + entry.getValue());
-//							    }
-							    
-							    for(Entry<String, ArrayList<BankBean>> entry : clientsStatements.entrySet()) {
-							    	  String key = entry.getKey();
-							    	  for(BankBean val : entry.getValue()) {
-							    	    // do something with key and each val
-							    		  System.out.println(val.getTransactions() + "  " + val.getBalance());
-							    	  }
-							    	}   
 
+								// System.out.println(clientsStatements.get(clientACnumber).listIterator());
+
+								// Set set = clientsStatements.entrySet();
+								// Iterator it = set.iterator();
+								// while (it.hasNext()) {
+								// Map.Entry entry = (Map.Entry) it.next();
+								// System.out.println(entry.getKey() + " : " +
+								// entry.getValue());
+								// }
+
+								int count = 1;
+								lastTransactions = "Transactions    Balance\n-------------------------\n";
+
+								for (Entry<String, ArrayList<BankBean>> entry : clientsStatements.entrySet()) {
+									String key = entry.getKey();
+
+									if (key.equals(clientACnumber)) {
+
+										int sizeValuesStatement = entry.getValue().size() - 10;
+
+										for (BankBean val : entry.getValue()) {
+
+											if (count > sizeValuesStatement) {
+												System.out.println(sizeValuesStatement + " " + key + "  "
+														+ val.getTransactions() + "  " + val.getBalance());
+												
+												lastTransactions += "\n" + val.getTransactions() + "     --->     " + val.getBalance();
+
+											}
+
+											count++;
+										}
+									}
+
+								}
+								
+								//sendMessage(lastTransactions);
 
 							}
-						
-						
+
 						} while (exit == false);
-						
-						
-						
-						
+
 					}
 
 					if (message.compareTo("2") == 0) {
@@ -390,15 +415,14 @@ class ClientServiceThread extends Thread {
 
 						sendMessage("Enter your Password");
 						clientPassword = (String) in.readObject();
-						
+
 						// Send values to the method
 						// to add arrayList
 						addCLientList(clientName, clientAddress, clientACnumber, clientUserName, clientPassword);
 
 						// Add the information into the file
-						appendFileLogin.println(clientName + "," + clientAddress + "," + clientACnumber + "," + clientUserName + "," + clientPassword);
-						
-			
+						appendFileLogin.println(clientName + "," + clientAddress + "," + clientACnumber + ","
+								+ clientUserName + "," + clientPassword);
 
 						appendFileLogin.close();
 						bw.close();
@@ -434,6 +458,6 @@ class ClientServiceThread extends Thread {
 
 	private void logment() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
